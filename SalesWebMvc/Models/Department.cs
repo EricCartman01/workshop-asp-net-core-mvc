fbf74d387890425b01e2435788717ca78f9340a1
Cartman <42188391+EricCartman01@users.noreply.github.com>
2020-06-30 23:05:42 -0300

    Other Entities and Second Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,9 +9,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int varX { get; set; }
-        public string Email { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
-        public string Description { get; set; }
+        public Department() 
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
